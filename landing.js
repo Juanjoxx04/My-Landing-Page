7366ffab9081042d32e01eaa69f8fc5d66163e5c
Juan José G.R <juanjoseg.r.r107@gmail.com>
2025-09-24 15:52:19 -0500

    subo archivos frontend de mi landing page

---
+++ landing.js
@@ -0,0 +1,62 @@
+export function controlClickAndScrollForNav() {
+  const navLinks = document.querySelectorAll(".nav-ul a");
+  const scrollSection = document.querySelectorAll("section");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      navLinks.forEach((l) => l.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    scrollSection.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.addEventListener.remove("active");
+      if (link.getAttribute("href") === "#" + current) {
+        link.classList.add("active");
+      }
+    });
+  });
+}
+
+export function detectedSections() {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  });
+
+  document.querySelectorAll(".sections-fade-in").forEach((el) => {
+    observer.observe(el);
+  });
+}
+
+export function messageAlert() {
+  const notWorking = document.getElementById("notWorking");
+  const modalAlert = document.getElementById("modalAlert");
+  const acceptButton = document.getElementById("acceptButton");
+
+  notWorking.addEventListener("click", function (e) {
+    e.preventDefault();
+    modalAlert.classList.add("show");
+    document.body.classList.add("noScroll");
+  });
+
+  acceptButton.addEventListener("click", function (e) {
+    modalAlert.classList.remove("show");
+    document.body.classList.remove("noScroll");
+  });
+}
